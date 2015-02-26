105d0c62e60f465da3fd128af33daf4b9777d5cb
kiotaku <kkutalol@gmail.com>
2015-02-26 16:00:24 +0900

    splash画面

--- src/com/example/android/STEPNavi/Splash.java
+++ src/com/example/android/STEPNavi/Splash.java
@@ -1,7 +1,30 @@
 package com.example.android.STEPNavi;
 
-/**
- * Created by crow on 2015/02/26.
- */
-public class Splash {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class Splash extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // タイトルを非表示にします。
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // splash.xmlをViewに指定します。
+        //setContentView(R.layout.splash);
+        Handler hdl = new Handler();
+        // 500ms遅延させてsplashHandlerを実行します。
+        hdl.postDelayed(new splashHandler(), 500);
+    }
+    class splashHandler implements Runnable {
+        public void run() {
+            // スプラッシュ完了後に実行するActivityを指定します。
+            Intent intent = new Intent(getApplication(), BluetoothChat.class);
+            startActivity(intent);
+            // SplashActivityを終了させます。
+            Splash.this.finish();
+        }
+    }
 }
